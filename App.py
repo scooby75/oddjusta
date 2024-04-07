21776fd616c78aa39614734762a8d5a9b53d46c2
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-04-07 10:43:41 -0300

    Update App.py

--- App.py
+++ App.py
@@ -140,7 +140,21 @@ file_paths = [
     "https://raw.githubusercontent.com/scooby75/bdfootball/main/azerbaijan-first-division-matches-2023-to-2024-stats.csv",
     "https://raw.githubusercontent.com/scooby75/bdfootball/main/cambodia-cambodian-league-matches-2021-to-2021-stats.csv",
     "https://raw.githubusercontent.com/scooby75/bdfootball/main/cambodia-cambodian-league-matches-2022-to-2022-stats.csv",
-    "https://raw.githubusercontent.com/scooby75/bdfootball/main/cambodia-cambodian-league-matches-2023-to-2023-stats.csv"
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/cambodia-cambodian-league-matches-2023-to-2023-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/chile-primera-division-matches-2021-to-2021-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/chile-primera-division-matches-2022-to-2022-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/chile-primera-division-matches-2023-to-2023-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/costa-rica-primera-division-fpd-matches-2021-to-2022-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/costa-rica-primera-division-fpd-matches-2022-to-2023-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/costa-rica-primera-division-fpd-matches-2023-to-2024-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/egypt-second-division-matches-2021-to-2022-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/egypt-second-division-matches-2022-to-2023-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/egypt-second-division-matches-2023-to-2024-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/greece-super-league-matches-2021-to-2022-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/greece-super-league-matches-2022-to-2023-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/greece-super-league-matches-2023-to-2024-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/hong-kong-hong-kong-premier-league-matches-2022-to-2023-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/hong-kong-hong-kong-premier-league-matches-2023-to-2024-stats.csv"
 
     
     
