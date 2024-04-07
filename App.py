691be3d33f3ed070981593180c2ad748cf949b95
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-04-07 15:22:04 -0300

    Update App.py

--- App.py
+++ App.py
@@ -142,8 +142,8 @@ def main():
 
 def mostrar_resultados(tipo_time, time, faixa_odds):
     if faixa_odds == "Outros":
-        odds_filtradas = df[df['Odd_Group'].apply(lambda x: float(x.split()[0])) < 1.0]
-        odds_filtradas = pd.concat([odds_filtradas, df[df['Odd_Group'].apply(lambda x: float(x.split()[-1])) > 6.0]])
+        odds_filtradas = df[df['Odd_Group'].apply(lambda x: float(x.split()[0]) if ' ' in x else -1) < 1.0]
+        odds_filtradas = pd.concat([odds_filtradas, df[df['Odd_Group'].apply(lambda x: float(x.split()[-1]) if ' ' in x else -1) > 6.0]])
     else:
         odds_filtradas = df[df['Odd_Group'] == faixa_odds]
 
