76ddf16c281882d48767ff6a2ceb49fb94b213b2
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-04-07 11:55:52 -0300

    Update App.py

--- App.py
+++ App.py
@@ -90,6 +90,39 @@ file_paths = [
     "https://www.football-data.co.uk/mmz4281/1617/SC0.csv", #Scotland Premier League
     "https://www.football-data.co.uk/mmz4281/1516/SC0.csv", #Scotland Premier League
     "https://www.football-data.co.uk/mmz4281/1415/SC0.csv", #Scotland Premier League
+
+    "https://www.football-data.co.uk/mmz4281/2324/SC1.csv", #Scotland Divison 1
+    "https://www.football-data.co.uk/mmz4281/2223/SC1.csv",
+    "https://www.football-data.co.uk/mmz4281/2122/SC1.csv",
+    "https://www.football-data.co.uk/mmz4281/2021/SC1.csv",
+    "https://www.football-data.co.uk/mmz4281/1920/SC1.csv",
+    "https://www.football-data.co.uk/mmz4281/1819/SC1.csv",
+    "https://www.football-data.co.uk/mmz4281/1718/SC1.csv",
+    "https://www.football-data.co.uk/mmz4281/1617/SC1.csv",
+    "https://www.football-data.co.uk/mmz4281/1516/SC1.csv",
+    "https://www.football-data.co.uk/mmz4281/1415/SC1.csv",
+
+    "https://www.football-data.co.uk/mmz4281/2324/SC2.csv", #Scotland Divison 2
+    "https://www.football-data.co.uk/mmz4281/2223/SC2.csv",
+    "https://www.football-data.co.uk/mmz4281/2122/SC2.csv",
+    "https://www.football-data.co.uk/mmz4281/2021/SC2.csv",
+    "https://www.football-data.co.uk/mmz4281/1920/SC2.csv",
+    "https://www.football-data.co.uk/mmz4281/1819/SC2.csv",
+    "https://www.football-data.co.uk/mmz4281/1718/SC2.csv",
+    "https://www.football-data.co.uk/mmz4281/1617/SC2.csv",
+    "https://www.football-data.co.uk/mmz4281/1516/SC2.csv",
+    "https://www.football-data.co.uk/mmz4281/1415/SC2.csv",
+
+    "https://www.football-data.co.uk/mmz4281/2324/SC3.csv", #Scotland Divison 3
+    "https://www.football-data.co.uk/mmz4281/2223/SC3.csv",
+    "https://www.football-data.co.uk/mmz4281/2122/SC3.csv",
+    "https://www.football-data.co.uk/mmz4281/2021/SC3.csv",
+    "https://www.football-data.co.uk/mmz4281/1920/SC3.csv",
+    "https://www.football-data.co.uk/mmz4281/1819/SC3.csv",
+    "https://www.football-data.co.uk/mmz4281/1718/SC3.csv",
+    "https://www.football-data.co.uk/mmz4281/1617/SC3.csv",
+    "https://www.football-data.co.uk/mmz4281/1516/SC3.csv",
+    "https://www.football-data.co.uk/mmz4281/1415/SC3.csv",
     
     "https://www.football-data.co.uk/mmz4281/2324/D1.csv", #Germany Bundesliga
     "https://www.football-data.co.uk/mmz4281/2223/D1.csv",
@@ -112,6 +145,8 @@ file_paths = [
     "https://www.football-data.co.uk/mmz4281/1617/D2.csv",
     "https://www.football-data.co.uk/mmz4281/1516/D2.csv",
     "https://www.football-data.co.uk/mmz4281/1415/D2.csv",
+
+    
     
     "https://www.football-data.co.uk/new/ARG.csv",
     "https://www.football-data.co.uk/new/AUT.csv",
@@ -178,7 +213,10 @@ file_paths = [
     "https://raw.githubusercontent.com/scooby75/bdfootball/main/south-korea-k-league-1-matches-2023-to-2023-stats.csv",
     "https://raw.githubusercontent.com/scooby75/bdfootball/main/south-korea-k-league-2-matches-2021-to-2021-stats.csv",
     "https://raw.githubusercontent.com/scooby75/bdfootball/main/south-korea-k-league-2-matches-2022-to-2022-stats.csv",
-    "https://raw.githubusercontent.com/scooby75/bdfootball/main/south-korea-k-league-2-matches-2023-to-2023-stats.csv"
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/south-korea-k-league-2-matches-2023-to-2023-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/thailand-thai-league-t1-matches-2021-to-2022-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/thailand-thai-league-t1-matches-2022-to-2023-stats.csv",
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/thailand-thai-league-t1-matches-2023-to-2024-stats.csv"
 
     
     
