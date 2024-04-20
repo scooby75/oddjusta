2f3937daa8c2d1e7dc06f42e2b37738d75af6ba0
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-04-20 03:42:22 -0300

    Update App.py

--- App.py
+++ App.py
@@ -35,8 +35,8 @@ def calcular_coeficiente(row, team_type):
 
 def agrupar_odd(odd):
     for i in range(1, 120):
-        lower = 1 + (i - 1) * 0.05
-        upper = 1 + i * 0.06
+        lower = 1 + (i - 1) * 0.06
+        upper = 1 + i * 0.05
         if lower <= odd <= upper:
             return f"{lower:.2f} - {upper:.2f}"
     return 'Outros'
