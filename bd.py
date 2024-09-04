6252a90c223a05f1f1da78c9e1bb8171f2acbc64
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-09-04 09:28:48 -0300

    Update bd.py

--- bd.py
+++ bd.py
@@ -2,6 +2,6 @@
 
 # Carregar os arquivos CSV
 file_paths = [
-    "https://raw.githubusercontent.com/scooby75/bdfootball/main/bd_geral_oddjusta.csv?token=GHSAT0AAAAAACQK5NGQIWZWKAFEGYQNMMX6ZQWTDWQ"
+    "https://raw.githubusercontent.com/scooby75/bdfootball/main/bd_geral_oddjusta.csv?token=GHSAT0AAAAAACUHURYJDNAQ3OQBQHYICRTGZWYKGWA"
     
 ]
